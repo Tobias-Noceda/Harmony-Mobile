c8dbb360ccb734144561688421c15a5505d380c9
Tobias-Noceda <tnoceda@itba.edu.ar>
2024-06-12 18:10:04 -0300

    Project now composable (don´t know why it wasn´t)

---
+++ app/src/main/java/ar/edu/itba/harmony_mobile/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package ar.edu.itba.harmony_mobile.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primary = Color(0xFF273043)
+val secondary = Color(0xFFE0E0E2)
+val tertiary = Color(0xFF2A9D8F)
