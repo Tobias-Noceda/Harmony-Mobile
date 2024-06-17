4ae74dfcd8cbd6791e3f37f8d095701f710456ac
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-06-17 18:12:05 -0300

    Data sources and repository added
    will adapt RoomDataSource in the future

--- app/src/main/java/ar/edu/itba/harmony_mobile/remote/model/RemoteError.kt
+++ app/src/main/java/ar/edu/itba/harmony_mobile/remote/model/RemoteError.kt
@@ -1,4 +1,4 @@
-package ar.edu.itba.example.api.remote.model
+package ar.edu.itba.harmony_mobile.remote.model
 
 import com.google.gson.annotations.SerializedName
 
---
+++ app/src/main/java/ar/edu/itba/harmony_mobile/remote/repository/DeviceRepository.kt
@@ -0,0 +1,41 @@
+package ar.edu.itba.harmony_mobile.remote.repository
+
+import ar.edu.itba.harmony_mobile.model.Device
+import ar.edu.itba.harmony_mobile.model.Lamp
+import ar.edu.itba.harmony_mobile.remote.DeviceRemoteDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class DeviceRepository(
+    private val remoteDataSource: DeviceRemoteDataSource
+) {
+    val devices: Flow<List<Device>> =
+        remoteDataSource.devices
+            .map { it.map { jt -> jt.asModel() } }
+
+    val currentDevice = devices.map { it.firstOrNull { jt -> jt is Lamp } }
+
+    suspend fun getDevice(deviceId: String): Device {
+        return remoteDataSource.getDevice(deviceId).asModel()
+    }
+
+    suspend fun addDevice(device: Device): Device {
+        return remoteDataSource.addDevice(device.asRemoteModel()).asModel()
+    }
+
+    suspend fun modifyDevice(device: Device): Boolean {
+        return remoteDataSource.modifyDevice(device.asRemoteModel())
+    }
+
+    suspend fun deleteDevice(deviceId: String): Boolean {
+        return remoteDataSource.deleteDevice(deviceId)
+    }
+
+    suspend fun executeDeviceAction(
+        deviceId: String,
+        action: String,
+        parameters: Array<*> = emptyArray<Any>()
+    ): Array<*> {
+        return remoteDataSource.executeDeviceAction(deviceId, action, parameters)
+    }
+}
