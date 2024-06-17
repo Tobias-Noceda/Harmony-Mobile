ef4bcf965379ccfa75b532c152ec3e010fe61769
Tobias-Noceda <tnoceda@itba.edu.ar>
2024-06-17 19:27:18 -0300

    Devices screen now navigable

--- app/src/main/java/ar/edu/itba/harmony_mobile/screens/DevicesScreen.kt
+++ app/src/main/java/ar/edu/itba/harmony_mobile/screens/DevicesScreen.kt
@@ -1,75 +1,162 @@
 package ar.edu.itba.harmony_mobile.screens
 
-import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.window.core.layout.WindowWidthSizeClass
+import ar.edu.itba.harmony_mobile.DeviceTypes
 import ar.edu.itba.harmony_mobile.R
+import ar.edu.itba.harmony_mobile.screens.devices.BlindsScreen
+import ar.edu.itba.harmony_mobile.screens.devices.LightScreen
+import ar.edu.itba.harmony_mobile.ui.theme.primary
+import ar.edu.itba.harmony_mobile.ui.theme.secondary
+import ar.edu.itba.harmony_mobile.ui.theme.tertiary
 
 @Composable
-fun DevicesScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+fun DevicesScreen(modifier: Modifier = Modifier, state: DeviceTypes? = null) {
+    var currentDestination by rememberSaveable { mutableStateOf(state) }
+
+    Box(modifier = modifier) {
+        if (currentDestination == null) {
+            DevicesList(
+                onDeviceClick = { deviceType ->
+                    currentDestination = deviceType
+                }
+            )
+        } else {
+            when (currentDestination) {
+                DeviceTypes.LIGHTS -> LightScreen()
+                DeviceTypes.DOORS -> LightScreen()
+                DeviceTypes.REFRIS -> LightScreen()
+                DeviceTypes.VACUUMS -> LightScreen()
+                DeviceTypes.SPRINKLERS -> LightScreen()
+                DeviceTypes.BLINDS -> BlindsScreen()
+                else -> {
+                    currentDestination = null
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DevicesList(onDeviceClick: (DeviceTypes) -> Unit) {
+    Column(modifier = Modifier.fillMaxSize()) {
         Text(
             text = stringResource(id = R.string.devices),
             modifier = Modifier.padding(
                 start = 12.dp,
-                top = 12.dp
+                top = 12.dp,
+                bottom = 12.dp
             ),
             style = MaterialTheme.typography.titleLarge
         )
 
         val scState = rememberScrollState(0)
+        val widthClass = currentWindowAdaptiveInfo().windowSizeClass.windowWidthSizeClass
+        val isCompact = widthClass == WindowWidthSizeClass.COMPACT
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(
                     horizontal = 6.dp,
-                    vertical = 12.dp
                 )
                 .verticalScroll(scState)
         ) {
-            for (deviceType in DeviceTypes.entries) {
-                Button(
-                    modifier = Modifier.padding(4.dp),
-                    shape = RoundedCornerShape(8.dp),
-                    onClick = {}
-                ) {
-                    Row (
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 12.dp),
-                        verticalAlignment = Alignment.CenterVertically
+            if(isCompact) {
+                for (deviceType in DeviceTypes.entries) {
+                    Button(
+                        modifier = Modifier.padding(4.dp),
+                        shape = RoundedCornerShape(8.dp),
+                        onClick = { onDeviceClick(deviceType) },
+                        colors = ButtonColors(secondary, primary, tertiary.copy(alpha = .5f), Color.White),
+                        elevation = ButtonDefaults.buttonElevation(8.dp),
+                        border = BorderStroke(
+                            width = 1.dp,
+                            color = Color.Black.copy(alpha = 0.3f)
+                        )
                     ) {
-                        Icon(
-                            deviceType.icon,
-                            contentDescription = null,
-                            modifier = Modifier.height(60.dp)
-                        )
+                        Row (
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 12.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                deviceType.icon,
+                                contentDescription = null,
+                                modifier = Modifier.height(60.dp)
+                            )
+                            Text(
+                                text = stringResource(id = deviceType.type),
+                                textAlign = TextAlign.Left,
+                                style = MaterialTheme.typography.titleMedium,
+                                modifier = Modifier.padding(start = 16.dp)
+                            )
+                        }
+                    }
+                }
+            } else {
+                val chunkedTypes = DeviceTypes.entries.chunked(2)
 
-                        Text(
-                            text = stringResource(id = deviceType.type),
-                            textAlign = TextAlign.Left,
-                            style = MaterialTheme.typography.titleMedium,
-                            modifier = Modifier.padding(start = 16.dp)
-                        )
+                for (chunk in chunkedTypes) {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        for (deviceType in chunk) {
+                            Button(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(4.dp),
+                                shape = RoundedCornerShape(8.dp),
+                                onClick = { onDeviceClick(deviceType) },
+                                colors = ButtonColors(secondary, primary, tertiary.copy(alpha = .5f), Color.White)
+                            ) {
+                                Row (
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 12.dp),
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Icon(
+                                        deviceType.icon,
+                                        contentDescription = null,
+                                        modifier = Modifier.height(60.dp)
+                                    )
+                                    Text(
+                                        text = stringResource(id = deviceType.type),
+                                        textAlign = TextAlign.Left,
+                                        style = MaterialTheme.typography.titleMedium,
+                                        modifier = Modifier.padding(start = 16.dp)
+                                    )
+                                }
+                            }
+                        }
                     }
                 }
             }
