a5a5c5e75f009a275da585065cfd383271861efa
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-06-17 20:15:34 -0300

    missed this part in home integration

--- app/src/main/java/ar/edu/itba/harmony_mobile/ApiApplication.kt
+++ app/src/main/java/ar/edu/itba/harmony_mobile/ApiApplication.kt
@@ -2,10 +2,12 @@ package ar.edu.itba.harmony_mobile
 
 import android.app.Application
 import ar.edu.itba.harmony_mobile.remote.DeviceRemoteDataSource
+import ar.edu.itba.harmony_mobile.remote.HomeRemoteDataSource
 import ar.edu.itba.harmony_mobile.remote.RoomRemoteDataSource
 import ar.edu.itba.harmony_mobile.remote.api.RetrofitClient
 import ar.edu.itba.harmony_mobile.repository.DeviceRepository
 import ar.edu.itba.harmony_mobile.remote.repository.RoomRepository
+import ar.edu.itba.harmony_mobile.repository.HomeRepository
 
 class ApiApplication  : Application() {
 
@@ -15,9 +17,15 @@ class ApiApplication  : Application() {
     private val deviceRemoteDataSource: DeviceRemoteDataSource
         get() = DeviceRemoteDataSource(RetrofitClient.deviceService)
 
+    private val homeRemoteDataSource: HomeRemoteDataSource
+        get() = HomeRemoteDataSource(RetrofitClient.homeService)
+
     val roomRepository: RoomRepository
         get() = RoomRepository(roomRemoteDataSource)
 
     val deviceRepository: DeviceRepository
         get() = DeviceRepository(deviceRemoteDataSource)
+
+    val homeRepository: HomeRepository
+        get() = HomeRepository(homeRemoteDataSource)
 }
