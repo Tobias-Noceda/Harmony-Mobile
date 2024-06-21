63023ff97a2aef795b13cf95f82d80a100343c25
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-06-21 15:54:29 -0300

    should be good enough

--- app/src/main/java/ar/edu/itba/harmony_mobile/remote/model/devices/RemoteLamp.kt
+++ app/src/main/java/ar/edu/itba/harmony_mobile/remote/model/devices/RemoteLamp.kt
@@ -1,18 +1,25 @@
 package ar.edu.itba.harmony_mobile.remote.model.devices
 
 import androidx.compose.ui.graphics.Color
+import androidx.core.graphics.toColorInt
 import ar.edu.itba.harmony_mobile.model.Lamp
 import ar.edu.itba.harmony_mobile.remote.model.RemoteStatus
 
 class RemoteLamp : RemoteDevice<RemoteLampState>() {
 
     override fun asModel(): Lamp {
+        var aux: Color;
+        try {
+            aux = Color(state.color.toLong(radix = 16))
+        }catch (e:Throwable){
+            aux = Color("#ffffffff".toColorInt())
+        }
         return Lamp(
             id = id,
             name = name,
             room = room?.asModel(),
             status = RemoteStatus.asModel(state.status),
-            color = Color(state.color.toLong(radix=16)),
+            color = aux,
             brightness = state.brightness
         )
     }
