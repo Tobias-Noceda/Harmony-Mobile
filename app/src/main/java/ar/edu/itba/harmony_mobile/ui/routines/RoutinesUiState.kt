a74699afca4e5b804d23e98a73e93b0de10293b3
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-06-20 23:34:26 -0300

    getRoutine

--- app/src/main/java/ar/edu/itba/harmony_mobile/ui/routines/RoutinesUiState.kt
+++ app/src/main/java/ar/edu/itba/harmony_mobile/ui/routines/RoutinesUiState.kt
@@ -10,6 +10,10 @@ data class RoutinesUiState(
     val routines: List<Routine> = emptyList(),
     val currentRoutine: Routine? = null
 ) {
+    fun getRoutine(routineId: String): Routine? {
+        return routines.find { it.id == routineId }
+    }
+
     fun getHomeRoutines(home: Home): List<Routine> {
         if (home.id == null || home.id == "0") {
             return routines.filter { it.homeId == "0" }
