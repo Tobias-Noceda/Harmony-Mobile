7251350d54b0a76a276a4455e604f0a3c0268210
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-06-22 00:05:08 -0300

    lord forgive me for I have sinned

--- app/src/main/java/ar/edu/itba/harmony_mobile/ui/devices/DevicesViewModel.kt
+++ app/src/main/java/ar/edu/itba/harmony_mobile/ui/devices/DevicesViewModel.kt
@@ -1,10 +1,13 @@
 package ar.edu.itba.harmony_mobile.ui.devices
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ar.edu.itba.harmony_mobile.DataSourceException
+import ar.edu.itba.harmony_mobile.model.Device
 import ar.edu.itba.harmony_mobile.repository.DeviceRepository
 import ar.edu.itba.harmony_mobile.model.Error
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -14,7 +17,7 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class DevicesViewModel(
-    repository: DeviceRepository
+    private val repository: DeviceRepository
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(DevicesUiState())
@@ -34,6 +37,27 @@ class DevicesViewModel(
             .distinctUntilChanged()
             .catch { e -> _uiState.update { it.copy(error = handleError(e)) } }
             .collect { response -> _uiState.update { updateState(it, response) } }
+    }
+
+    fun getDevice(deviceId: String) {
+        runOnViewModelScope(
+            { repository.getDevice(deviceId) },
+            { state, response -> state.copy(currentDevice = response) }
+        )
+    }
+
+    private fun <R> runOnViewModelScope(
+        block: suspend () -> R,
+        updateState: (DevicesUiState, R) -> DevicesUiState
+    ): Job = viewModelScope.launch {
+        _uiState.update { it.copy(isFetching = true, error = null) }
+        runCatching {
+            block()
+        }.onSuccess { response ->
+            _uiState.update { updateState(it, response).copy(isFetching = false) }
+        }.onFailure { e ->
+            _uiState.update { it.copy(isFetching = false, error = handleError(e)) }
+        }
     }
 
     private fun handleError(e: Throwable): Error {
