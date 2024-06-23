e0652e6414fecef5265e14b253fb02303a0155b6
Tobias-Noceda <tnoceda@itba.edu.ar>
2024-06-23 15:39:08 -0300

    Hosted api now default

--- app/src/main/java/ar/edu/itba/harmony_mobile/remote/api/GlobalAPIData.kt
+++ app/src/main/java/ar/edu/itba/harmony_mobile/remote/api/GlobalAPIData.kt
@@ -2,8 +2,8 @@ package ar.edu.itba.harmony_mobile.remote.api
 
 class GlobalAPIData {
     companion object {
-//         const val API_URL = "http://10.0.2.2:8080/api/" // if you are using an emulator
-        //  const val API_URL = "http://192.168.0.24:8080/api/" // if you are not using an emulator
+        // const val API_URL = "http://10.0.2.2:8080/api/" // if you are using an emulator
+        // const val API_URL = "http://192.168.0.24:8080/api/" // if you are not using an emulator
         const val API_URL = "https://api-hci-9716.onrender.com/api/" //if you want the hosted api
     }
 }
